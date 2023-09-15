00f9c806d1ed9224364d40ece6fb4af85c32a78a
Amanda Lobato <amandalobatonascimento@gmail.com>
2023-09-15 11:03:12 -0300

    aula5

--- app.js
+++ app.js
@@ -35,3 +35,41 @@ app.listen(8081, function(){
     console.log("Servidor Ativo!")
 })
 
+app.get("/consulta", function(req, res){
+    post.findAll().then(function(post){
+        res.render("consulta", {post})
+    }).catch(function(erro){
+        consola.log("Erro ao carregar dados do banco: " + erro)
+    })
+})
+
+app.get("/excluir/:id", function(req, res){
+    post.destroy({where: {'id': req.params.id}}).then(function(){
+        res.render("primeira_pagina")
+    }).catch(function(erro){
+        console.log("Erro ao excluir ou encontrar os dados do banco: " + erro)
+    })
+})
+
+app.get("/editar/:id", function(req, res){
+    post.findAll({where: {'id': req.params.id}}).then(function(post){
+        res.render("editar", {post})
+    }).catch(function(erro){
+        console.log("Erro ao excluir ou encontrar os dados do banco: " + erro)
+    })
+})
+
+app.post("/atualizar", function(req, res){
+    post.update({
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        bairro: req.body.bairro,
+        cep: req.body.cep
+    },{
+        where: {
+            id: req.body.id
+        }
+    }).then(function(){
+        res.redirect("/consulta")
+    })
+})
